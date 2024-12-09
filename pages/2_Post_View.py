f668dda3a40edbcb3ed35a5842d8d555f854ddef
Shaun <158432022+unthinkableETH@users.noreply.github.com>
2024-12-08 21:48:47 -0500

    Update 2_Post_View.py

--- pages/2_Post_View.py
+++ pages/2_Post_View.py
@@ -368,7 +368,7 @@ try:
                 current_comment = comment_dict.get(current_comment['parent_id'])
             
             if highlighted_chain:
-                st.markdown("### Comment Thread Context")
+                st.header("Comment Thread Context")
                 
                 for i, comment in enumerate(highlighted_chain):
                     is_highlighted = comment['id'] == highlight_comment_id
